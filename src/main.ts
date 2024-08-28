f71d91b1d3cf39d200e13db35d181060e73f6d12
Andrii Klimkovskyi <andrew.klimkovskyi@gmail.com>
2024-08-29 00:39:32 +0300

    correct main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,1 @@
 import "./style.css";
-
-type User = {
-  name: string;
-  surname: string;
-  email: string;
-  password: string;
-};
-
-function createOrUpdateUser(initialValues: Partial<User>): void {
-  console.log(initialValues);
-}
-
-createOrUpdateUser({
-  email: "user@mail.com",
-  password: "password123",
-});
